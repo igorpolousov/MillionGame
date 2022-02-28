f8cfdb3ae89b47705a96d214da270eb5e07f8740
Igor Polousov <igorpolousov@gmail.com>
2022-02-28 13:19:29 +0300

    Initial Commit

---
+++ MillionGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MillionGame
+//
+//  Created by Igor Polousov on 28.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
