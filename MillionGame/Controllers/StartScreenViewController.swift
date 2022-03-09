e8f4fabf1591ce785063513c5cd7f18cef47a58b
Igor Polousov <igorpolousov@gmail.com>
2022-03-09 15:22:58 +0300

    + outlets

---
+++ MillionGame/Controllers/StartScreenViewController.swift
@@ -0,0 +1,23 @@
+//
+//  StartScreenViewController.swift
+//  MillionGame
+//
+//  Created by Igor Polousov on 09.03.2022.
+//
+
+import UIKit
+
+class StartScreenViewController: UIViewController {
+    
+    @IBAction func gameStart(_ sender: Any) {
+    }
+    
+    @IBOutlet var userResultsLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
---
+++ MillionGame/Controllers/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  MillionGame
+//
+//  Created by Igor Polousov on 28.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var questionLabel: UILabel!
+    
+    @IBOutlet var answerButton1: UIButton!
+    @IBOutlet var answerButton2: UIButton!
+    @IBOutlet var answerButton3: UIButton!
+    @IBOutlet var answerButton4: UIButton!
+    
+    @IBAction func heplButton(_ sender: Any) {
+    }
+    @IBAction func halfButton(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
